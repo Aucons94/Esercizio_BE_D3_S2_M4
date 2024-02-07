de1e849e6aa666ac7fb6baef42c55d3e70d25ca4
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-02-07 17:11:03 +0100

    Initial commit

---
+++ Esercizio3.sql
@@ -0,0 +1,25 @@
+-- SELECT COUNT(*) from Orders
+
+-- SELECT COUNT(*) from Employees
+
+-- SELECT COUNT(*) from Employees Where City='London'
+
+-- SELECT AVG(Freight) From Orders
+
+-- SELECT AVG(Freight) From Orders Where CustomerID='BOTTM'
+
+-- SELECT CustomerID, SUM(Freight) FROM Orders Group by CustomerID
+
+-- SELECT COUNT(*), City FROM Customers Group by City
+
+-- SELECT OrderId, SUM((UnitPrice*Quantity)) As TotaleRiga From [Order Details] Group by OrderID
+
+-- SELECT OrderId, SUM((UnitPrice*Quantity)) As TotaleRiga From [Order Details] Group by OrderID Having OrderID=10248
+
+-- SELECT Count(*), CategoryID From Products Group by CategoryID
+
+-- Select Count(*), ShipCountry From Orders Group by ShipCountry
+
+-- Select AVG(Freight), ShipCountry From Orders Group by ShipCountry
+
+
